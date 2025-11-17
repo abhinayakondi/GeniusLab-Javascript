7784a0ecec26639944de7dd8f4aa40153f79d031
abhinayakondi <abhisonukondi123@gmail.com>
2025-11-16 23:34:53 -0500

    hoisting in JS

--- #2 JavaScript Variables and Data Types/hoisting.js
+++ #2 JavaScript Variables and Data Types/hoisting.js
@@ -8,6 +8,12 @@ var a = 10;
 
 // b is hoisted BUT NOT initialized
 // b is in the "Temporal Dead Zone" (TDZ)
+// Accessing a variable before it's declaration results is a Temporal Dead Zone, 
+// Temporal Dead Zone prevents access to uninitialized variables.
 console.log(b); // ❌ ReferenceError
 let b = 20;
+
+// Destructuring Assignment with let
+let { name, age } = { name: "Pranjal", age: 20 };
+console.log(name, age);
 
