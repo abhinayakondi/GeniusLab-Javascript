d567815daccc66c6c88b6049a2f45ef0ca95417c
abhinayakondi <abhisonukondi123@gmail.com>
2025-11-19 17:05:25 -0500

    Quick reference on all primitive data types in JS

---
+++ #2 JavaScript Variables and Data Types/Primitive_datatypes.js
@@ -0,0 +1,60 @@
+// JavaScript does not need to be declared with a specific data type
+// Numbers
+
+let n1 = 2;
+console.log(n1)
+
+let n2 = 1.3;
+console.log(n2)
+
+let n3 = Infinity;
+console.log(n3)
+
+let n4 = 'something here too' / 2;
+console.log(n4)
+
+// Strings
+
+let s1 = "Hello There";
+console.log(s1); 
+
+let s2 = 'Single quotes work fine';
+console.log(s2); 
+
+let s3 = `can embed ${s1}`;
+console.log(s3);
+
+// Booleans
+
+let b1 = true;
+console.log(b1);  
+
+let b2 = false;
+console.log(b2);
+
+// Null
+
+let age = null;
+console.log(age)
+
+// Undefined
+
+let a;
+console.log(a);
+
+// BigInt
+// Used for integers larger than 2^53 - 1
+// The largest number in JS that can be safely represented using Number primitive is 9007199254740991 (2^53 - 1) 
+// known as MAX_SAFE_INTEGER
+let b = BigInt("0b1010101001010101001111111111111111");
+console.log(b);
+
+const MaxBigInt = Number.MAX_SAFE_INTEGER;
+console.log(MaxBigInt);
+
+// Symbol
+// Used to create unique identifiers
+let sym1 = Symbol("Geeks");
+let sym2 = Symbol("Geeks");
+console.log(sym1 == sym2);
+
