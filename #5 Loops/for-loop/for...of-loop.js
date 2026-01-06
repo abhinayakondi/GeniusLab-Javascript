58d5fe41a7767236840beda7b3766318a0580767
abhinayakondi <abhisonukondi123@gmail.com>
2026-01-05 23:24:29 -0500

    for...of loop with iterable objects

--- #5 Loops/for-loop/for...of-loop.js
+++ #5 Loops/for-loop/for...of-loop.js
@@ -6,7 +6,60 @@
 //for...in loop is better suited for objects.
 
 // Used in Arrays
+
+const a = [ 1, 2, 3, 4, 5 ];
+
+for (let item of a){
+    console.log(item);
+}
+console.log();
+
 // Used in Strings
+const str = "Hello";
+
+for (let char of str){
+    console.log(char);
+}
+console.log();
+
 // Used in Maps
+const m = new Map([
+    ["name", "Abi"],
+    ["age", 21],
+    ["city", "Toronto"]
+]);
+
+console.log("With interpolation");
+for (let [key, value] of m){
+    console.log(`${key},${value}`);// ` (backticks) used for string interpolation
+    // Interpolation = embed variables or expressions inside a string automatically.
+}
+
+console.log("Without interpolation");
+for (let [key, value] of m){
+    //Older version or withour interpolation
+    console.log(key + "," + value);
+}
+console.log();
+
 // Used in Sets
+let s= new Set([1,2,3,4,5]);
+
+for (let val of s){
+    console.log(val);
+}
+console.log();
+
 // Used in Objects
+let ob={
+    name: 'Abi',
+    age: 21,
+    city: 'Toronto'
+};
+
+for(let fob of Object.keys(ob)){
+    console.log(`${fob}: ${ob[fob]}`); // String interpolation
+}
+console.log();
+
+// Here Object.keys(), Object.values(), or Object.entries() are keywords and cannot be replaced
