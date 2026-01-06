c1f22d942c59bad0a7eb3ec0650aacaf6814d25d
abhinayakondi <abhisonukondi123@gmail.com>
2026-01-05 21:02:35 -0500

    Update in loops. First push of 26'and more to gogit push

--- #5 Loops/do...while-loop.js
+++ #5 Loops/do...while-loop.js
@@ -6,3 +6,13 @@ do {
     console.log("Iteration:", i);
     i++;
 } while (i < 3);
+
+// do...while executed at least once even if the condition is false
+let test = 1;
+do {
+    console.log(test);
+} while(test<1)
+
+while(test<1){
+    console.log(test);
+} // this will not execute
--- #5 Loops/while-loop.js
+++ #5 Loops/while-loop.js
@@ -7,3 +7,12 @@ while (i < 3) {
     console.log("Number:", i);
     i++;
 }
+
+//To traverse an array using while loop
+let arr = [10, 20, 30, 40];
+let j = 0;
+while (j < arr.length) {
+  console.log(arr[i]);
+  i++;
+}
+
