4b41e876491251d65526ad3d61ff7a09a983a298
abhinayakondi <abhisonukondi123@gmail.com>
2026-01-06 18:19:40 -0500

    Created file for break, continue and errors

--- #5 Loops/for-loop/for-each-loop.js
+++ #5 Loops/for-loop/for-each-loop.js
@@ -7,18 +7,27 @@
 // Another function here is -> forEach(...)
 // JavaScript treats functions as first-class citizens, so you can pass them around like variables.
 
+// You can think of forEach as not a for-loop
+// but a function (built in array method) that takes another function as an argument
+// and calls that function within for each element of the array.
 
 //for each element in an array
 const numbers = [10, 20, 30];
 
 // original declaration
 console.log("---- Original Declaration ----");
-numbers.forEach(function func(num) { console.log(num)}); // function within a function
+numbers.forEach(function func(num) 
+{ 
+  console.log(num);
+}); // function within a function
 
 
 //works without a function name declaration too
 console.log("---- Without function name ----");
-numbers.forEach(function (num) { console.log(num)});
+numbers.forEach(function(num) 
+{ 
+  console.log(num);
+});
 
 // can also be written using arrow function syntax
 console.log("---- Using Arrow Function ----");
@@ -30,3 +39,10 @@ let arr = [10, 20, 30];
 for (let i = 0; i < arr.length; i++) {
   console.log(arr[i]); 
 }
+
+//forEach with indexing
+console.log("---- forEach with Indexing ----");
+numbers.forEach(function(num, index) 
+{ 
+  console.log("Index:"+index+" Value:"+num);
+});
