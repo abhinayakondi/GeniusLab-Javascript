66069d41e6322a80b26e33a379ea8b7a564e98f5
abhinayakondi <abhisonukondi123@gmail.com>
2026-01-05 21:23:43 -0500

    for...of comments

---
+++ #5 Loops/for-loop/for...of-loop.js
@@ -0,0 +1,12 @@
+//For iterables ,  the below loops are preferred.
+//iterable objects such as arrays, strings, maps, sets:
+
+//for...of Loop if we need to put continue or break in the loop 
+//forEach() if we need execute something for all elements without any condition
+//for...in loop is better suited for objects.
+
+// Used in Arrays
+// Used in Strings
+// Used in Maps
+// Used in Sets
+// Used in Objects
