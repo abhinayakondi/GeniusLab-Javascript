65872ba8802cf9beb69c3977a0e959c77af53f99
abhinayakondi <abhisonukondi123@gmail.com>
2026-01-08 13:06:55 -0500

    Errors in JS

--- #5 Loops/errors-throw-try&catch.js
+++ #5 Loops/errors-throw-try&catch.js
@@ -6,6 +6,7 @@ try {
 catch (err) {
     console.log(err);
 }
+console.log();
 
 // The throw statement allows you to create custom error messages and throw exceptions manually.
 // Earlier it was a ReferenceError because dadalert is not defined as described by the compiler.
@@ -16,4 +17,42 @@ try {
 }
 catch (e) { // here e or err are just variable names and not keywords
     console.log(e);
+}
+console.log();
+
+// The finally block executes unconditionally after the try and catch blocks, 
+// regardless of whether an error was thrown or caught.
+
+try {
+    console.log("try"); // this will run without any error
+}
+catch (e) {
+    console.log("catch"); // since there is no error, this block will be skipped
+}
+finally {
+    console.log("finally");
+}
+console.log();
+
+// Another example where an error is thrown and caught
+
+try{
+    throw new Error("This is a custome error message ;)");
+}
+catch(e){
+    console.log("Caught an error: " + e);
+}
+finally{
+    console.log("Execution completed.");        
+}
+console.log();
+
+//============================================================
+// Creating custome error types by extending the Error class
+//============================================================
+function ValidateInput(value) {
+    if (value<0){
+        throw new ValidationError("Input must be a non-negative number.");
+    }
+    return "Valid input";
 }
