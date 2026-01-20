127a91bf39e8268c5da96d440a7e130133349693
abhinayakondi <abhisonukondi123@gmail.com>
2026-01-20 18:36:54 -0500

    Update

--- #6 Functions/Types_of_functions/#04_arrow_function.js
+++ #6 Functions/Types_of_functions/#04_arrow_function.js
@@ -6,17 +6,28 @@
 
 // For learning and for simple code, arrow functions are kinda unnecessary.
 // They exist for convenience, not because JavaScript couldn’t work without them.
+// Implicit return for single expressions
 
+//Syntax:
+// const functionName = (parameters) => {  // function body  return result;};
+
+//No parameters
+console.log();
 const greet = () => "Hi there!";
 console.log(greet()); // Hi there!
 
+// Single parameter
+console.log();
 const square = (n) => n * n;
 console.log(square(4)); // 16
 
+// Multiple parameters
+console.log();
 const add = (a, b) => a + b;
 console.log(add(2, 5)); // 8
 
 // Use braces {} for multiple statements
+console.log();
 const great = (a, b) => {
     if (a > b)
         return "a is greater";
@@ -24,6 +35,14 @@ const great = (a, b) => {
         return "b is greater";
 }
 console.log(great(3, 5));
+
+//Object creation
+console.log();
+const obj = (firstname, lastname) => ({first:firstname, last:lastname});
+let student1= obj("Abi", "Kondi");
+let student2= obj("Sonu", "Kondi");
+console.log(student1);
+console.log(student2);
 
 // Function Declaration: Can be called anywhere in the scope
 // Function Expression: Can only be called after the line where it is defined
