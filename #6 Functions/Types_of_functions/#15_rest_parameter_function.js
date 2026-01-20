daf81940f0cf06334f02bf9d3e985ec9241f9f19
abhinayakondi <abhisonukondi123@gmail.com>
2026-01-20 14:06:33 -0500

    Update rest parameters

--- #6 Functions/Types_of_functions/#15_rest_parameter_function.js
+++ #6 Functions/Types_of_functions/#15_rest_parameter_function.js
@@ -1,5 +1,8 @@
 // Useful when you don't know how many arguments will be passed to the function.
 // Take all arguments passed to this function and put them into an array called nums.
+// Must always be the last parameter in a function
+// Placing any parameter after rest parameter will result in syntax error
+// function myFun(a, b, ...nums , c) { // Syntax Error
 
 // Regular array calculation
 function add(...numbers) {
@@ -14,6 +17,19 @@ function add(...numbers) {
 console.log(add(1, 2, 3, 4, 5)); // 15
 
 //----------------------------------------------------------------
+// Using rest parameter with other parameters
+// rest with function and other arguments
+console.log();
+function fun(a, b, ...c) {
+    console.log(`${a} ${b}`); //Mukul, Latiyan
+    console.log(c);  //[ 'Lionel', 'Messi', 'Barcelona' ]
+    console.log(c[0]); //Lionel
+    console.log(c.length); //3
+    console.log(c.indexOf('Lionel')); //0
+}
+fun('Mukul', 'Latiyan', 'Lionel', 'Messi', 'Barcelona');
+
+//----------------------------------------------------------------
 // Using reduce 
 
 /*
@@ -21,6 +37,7 @@ reduce() method combines all values in an array into one value.
 (a, b) => a + b → how to combine two numbers
 0 → starting value
 */
+console.log();
 function sum(...nums){
     let total = nums.reduce( (a,b)=>a+b , 0 );
     return total;
